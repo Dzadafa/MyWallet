a6632435039dc4a4c96f95a2e3ee28ea159db66c
Dzadaafa <dzadafa77@gmail.com>
2025-11-01 23:17:05 +0700

    Update: Replaced the wishlist to be checked, instead of getting deleted

--- app/src/main/java/com/dzadafa/mywallet/data/WishListItem.kt
+++ app/src/main/java/com/dzadafa/mywallet/data/WishListItem.kt
@@ -5,5 +5,6 @@ import com.google.firebase.firestore.DocumentId
 data class WishlistItem(
     @DocumentId val id: String? = null,
     val name: String = "",
-    val price: Double = 0.0
+    val price: Double = 0.0,
+    val completed: Boolean = false
 )
