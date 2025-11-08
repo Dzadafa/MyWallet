504f8a1b834e1fd3465dbb6b054c69705027b7e4
Dzadaafa <dzadafa77@gmail.com>
2025-11-08 17:37:58 +0700

    add: Notification

--- app/src/main/java/com/dzadafa/mywallet/MyWalletApplication.kt
+++ app/src/main/java/com/dzadafa/mywallet/MyWalletApplication.kt
@@ -1,6 +1,10 @@
 package com.dzadafa.mywallet
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import com.dzadafa.mywallet.data.AppDatabase
 import com.dzadafa.mywallet.data.TransactionRepository
 import com.dzadafa.mywallet.data.WishlistRepository
@@ -15,5 +19,20 @@ class MyWalletApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         ThemeManager.applyTheme(this)
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Transactions"
+            val descriptionText = "Notifications for new transactions"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("TRANSACTION_CHANNEL", name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
